756310d867c9c5354bebd19bf01491208e8d277e
Dipta-17 <diptad170@gmail.com>
2024-05-19 13:54:36 +0530

    1st prog

---
+++ 1st.py
@@ -0,0 +1,9 @@
+#write a program in python using arithmetic operator
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+print("the sum of two numbers",a+b) 
+print("the subtraction of two numbers",a-b) 
+print("the multiplication of two numbers",a*b) 
+print("the division of two numbers",a//b) 
+print("the floor division of two numbers",a/b)
+print("the modulus division of two numbers",a%b)
