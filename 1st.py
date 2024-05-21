f9bb394bb88ba6067a9c2d80c3c9043bf9979870
Dipta-17 <152770104+Dipta-17@users.noreply.github.com>
2024-05-21 11:19:45 +0530

    Delete 1st.py

--- 1st.py
+++
@@ -1,9 +0,0 @@
-#write a program in python using arithmetic operator
-a=int(input("Enter first number:"))
-b=int(input("Enter second number:"))
-print("the sum of two numbers",a+b) 
-print("the subtraction of two numbers",a-b) 
-print("the multiplication of two numbers",a*b) 
-print("the division of two numbers",a//b) 
-print("the floor division of two numbers",a/b)
-print("the modulus division of two numbers",a%b)
